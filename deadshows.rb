4eea5a60a57bf67a4eae78e29b2b1aadc9696963
Jonathan Julian <jonathan@jonathanjulian.com>
2022-08-11 23:18:45 -0400

    add imagemagick manipulation
    WIP, obviously, but the image -> annotated album cover prototype works!

--- deadshows.rb
+++ deadshows.rb
@@ -7,26 +7,66 @@ require 'date'
 # GoogleSearch.api_key = ENV["SERP_API_KEY"]
 # query = "1970-04-03 April 03, 1970 Armory Fieldhouse, University of Cincinnati, Cincinnati, OH"
 # search = GoogleSearch.new(
-#     q: query, 
-#     tbm: 'isch'
+#   q: query, 
+#   tbm: 'isch'
 # )
 # json_results = search.get_json
 # File.write("#{query}-#{Time.now.to_i}.json", json_results)
 
-json_results = JSON.parse(File.read('./1970-04-03 April 03, 1970 Armory Fieldhouse, University of Cincinnati, Cincinnati, OH-1660186215.json'))
-json_results["images_results"].each do |image|
-    if image["source"] =~ /grateful|dead|jerrygarcia|liveforlivemusic|flickr/
-        puts image["original"]
-    end
+# json_results = JSON.parse(File.read('./1970-04-03 April 03, 1970 Armory Fieldhouse, University of Cincinnati, Cincinnati, OH-1660186215.json'))
+# json_results["images_results"].each do |image|
+#   if image["source"] =~ /grateful|dead|jerrygarcia|liveforlivemusic|flickr/
+#     puts image["original"]
+#   end
+# end
+
+require 'rmagick'
+include Magick
+
+fnames = [
+  '04-03-70.jpg', '19700403_0396.jpg'
+]
+target_cols = 500 # how wide we want the final image
+target_rows = 425 # how much space to leave at the bottom
+fnames.each do |file|
+  puts file
+  img = Image.read(file).first
+  if img.columns < target_cols || img.rows < target_rows # not perfect; small images will crop horizontally
+    puts "too small"
+    next
+  end
+
+  # keep the image aspect, but make it as tall as the image area
+  img.change_geometry!("x#{target_rows}") { |cols, rows, i|
+    puts cols, rows
+    i.resize!(cols, rows)
+  }
+
+  # create a new white image and overlay the image from above
+  white_bg = Image.new(target_cols, target_cols) { |options| options.background_color = 'white' }
+  target = white_bg.composite(img, NorthGravity, OverCompositeOp)
+
+  # annotate with text
+  text = Draw.new
+  text.pointsize = 20
+  text.font = '/Users/jjulian/Library/Fonts/ChewedPenBB.otf'
+  text.gravity = WestGravity
+  text.fill = '#151B54' # ballpoint pen blue
+  text_row = 202
+  x_margin = 10
+  text_height = 24
+  text.annotate(target, 0, 0, x_margin, text_row, "Armory Fieldhouse, University of Cincinnati, Cincinnati, OH")
+  text.annotate(target, 0, 0, x_margin, text_row + text_height, "April 3, 1970")
+
+  target.write("final-#{file}")
 end
 
 
-exit
 
-# https://github.com/MichaelAdamBerry/darkstar-project/tree/master/data
-data = JSON.parse(File.read('./gratefulSetlist.json'))
-data['setlists'].each do |show|
-    date = Date.strptime(show['eventDate'], '%d-%m-%Y')
-    catalog_date = "#{date.year}-#{'%02d' % date.month}-#{'%02d' % date.day}"
-    puts "#{catalog_date} #{date.strftime('%B %d, %Y')} #{show['venue']['name']}, #{show['venue']['city']['name']}, #{show['venue']['city']['stateCode']}"
-end
+# # https://github.com/MichaelAdamBerry/darkstar-project/tree/master/data
+# data = JSON.parse(File.read('./gratefulSetlist.json'))
+# data['setlists'].each do |show|
+#   date = Date.strptime(show['eventDate'], '%d-%m-%Y')
+#   catalog_date = "#{date.year}-#{'%02d' % date.month}-#{'%02d' % date.day}"
+#   puts "#{catalog_date} #{date.strftime('%B %d, %Y')} #{show['venue']['name']}, #{show['venue']['city']['name']}, #{show['venue']['city']['stateCode']}"
+# end
